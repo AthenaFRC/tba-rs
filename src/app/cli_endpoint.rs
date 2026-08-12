49a3da57856f0655fe4d22a2a77880b08d33d6c9
Trevor Sears <trevor@trevorsears.com>
2026-08-12 16:42:32 -0400

    Renamed some endpoint names in the CLI.

--- src/app/cli_endpoint.rs
+++ src/app/cli_endpoint.rs
@@ -4,6 +4,7 @@
 pub enum CLIEndpoint {
 	/// Gets a list of DCMP events and awards for the given district.
 	/// abbreviation.
+	#[command(name = "district/dcmp-history")]
 	DistrictDCMPHistory {
 		/// The abbreviated district name (e.g. `ne` or `fim`).
 		district_abbreviation: String,
@@ -11,18 +12,21 @@ pub enum CLIEndpoint {
 
 	/// Gets a list of District objects with the given district abbreviation.
 	/// This accounts for district abbreviation changes, such as MAR to FMA.
+	#[command(name = "district/history")]
 	DistrictHistory {
 		/// The abbreviated district name (e.g. `ne` or `fim`).
 		district_abbreviation: String,
 	},
 
 	/// Gets insights for a given district.
+	#[command(name = "district/insights")]
 	DistrictInsights {
 		/// The abbreviated district name (e.g. `ne` or `fim`).
 		district_abbreviation: String,
 	},
 
 	/// Gets a list of advancement information per team in a district.
+	#[command(name = "district/advancement")]
 	DistrictAdvancement {
 		/// The TBA district key (e.g. `2016fim`).
 		district_key: String,
