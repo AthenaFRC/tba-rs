75375363bee60d30a6ea7b1a70770734c2128ffb
Trevor Sears <trevor@trevorsears.com>
2026-08-13 01:15:37 -0400

    Implemented print function for serde serializables.

--- src/app/handlers/get_endpoint.rs
+++ src/app/handlers/get_endpoint.rs
@@ -1,11 +1,9 @@
-use crate::{
-	APIClient,
-	api,
-	app::scaffolding::{
-		CLIEndpoint,
-		OutputFormat,
-	},
-};
+use serde_json::ser::PrettyFormatter;
+use serde_json::Serializer;
+use crate::{app::scaffolding::{
+	CLIEndpoint,
+	OutputFormat,
+}, APIClient, APIResult, api};
 
 pub async fn get_endpoint(
 	endpoint: CLIEndpoint,
@@ -55,4 +53,40 @@ pub async fn get_endpoint(
 	// };
 
 	Ok(())
+}
+
+fn print_result<T: serde::Serialize>(
+	result: APIResult<T>,
+	include_e_tag: bool,
+	format: OutputFormat,
+) -> Result<(), Box<dyn std::error::Error>> {
+	match result {
+		APIResult::NotModified => println!("Not modified. (ETag matched)"),
+		APIResult::Unauthorized => eprintln!("Authorization failed. Ensure your API key is valid."),
+		APIResult::Err(message) => eprintln!("Error: {}", message),
+		APIResult::Ok { result, e_tag } => {
+			println!("{}", match format {
+				OutputFormat::JSON => serde_json::to_string(&result)?,
+				OutputFormat::JSONPrettyTabs => get_pretty_json_string(&result, b"\t")?,
+				OutputFormat::JSONPretty2Spaces => get_pretty_json_string(&result, b"  ")?,
+				OutputFormat::JSONPretty4Spaces => get_pretty_json_string(&result, b"    ")?,
+				OutputFormat::JSONL => "".to_string(), // TODO: Implement JSONL output
+				OutputFormat::CSV => "".to_string(), // TODO: Implement CSV output
+				OutputFormat::TSV => "".to_string(), // TODO: Implement TSV output
+			});
+			if include_e_tag { println!("ETag: {}", e_tag); }
+		},
+	};
+	Ok(())
+}
+
+fn get_pretty_json_string<T: serde::Serialize>(
+	value: &T,
+	indentation: &[u8],
+) -> Result<String, Box<dyn std::error::Error>> {
+	let formatter = PrettyFormatter::with_indent(indentation);
+	let mut buffer = Vec::new();
+	let mut serializer = Serializer::with_formatter(&mut buffer, formatter);
+	value.serialize(&mut serializer)?;
+	Ok(String::from_utf8(buffer)?)
 }
