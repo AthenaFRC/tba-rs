e91485e5219178459dd6a70bc576962917bb9b3f
Trevor Sears <trevor@trevorsears.com>
2026-08-13 01:14:56 -0400

    Fixed enum variant names in `OutputFormat`.

--- src/app/scaffolding/output_format.rs
+++ src/app/scaffolding/output_format.rs
@@ -12,12 +12,12 @@ pub enum OutputFormat {
 	/// Pretty-printed JSON format, including line breaks and indentation via 2
 	/// spaces.
 	#[value(name = "json-pretty-2spaces")]
-	JSONPretty2spaces,
+	JSONPretty2Spaces,
 
 	/// Pretty-printed JSON format, including line breaks and indentation via 4
 	/// spaces.
 	#[value(name = "json-pretty-4spaces")]
-	JSONPretty4spaces,
+	JSONPretty4Spaces,
 
 	/// JSON Lines format. Each line of the resulting output will be a valid
 	/// JSON object.
