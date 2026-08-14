615e7d05b67cdb29bc962022b5c4571fe9ef44ef
Trevor Sears <trevor@trevorsears.com>
2026-08-14 16:07:40 -0400

    Replaced default `help` and `version` flag descriptions.

--- src/app/scaffolding/tba_command.rs
+++ src/app/scaffolding/tba_command.rs
@@ -4,11 +4,15 @@ use crate::app::scaffolding::TBASubcommand;
 
 #[derive(clap::Parser, Debug)]
 #[command(
-	version,
 	about,
+	version,
+	propagate_version = true,
 	max_term_width = 120,
 	arg_required_else_help = true,
-	help_expected = true
+	help_expected = true,
+	disable_version_flag = true,
+	disable_help_flag = true,
+	disable_help_subcommand = true
 )]
 pub struct TBACommand {
 	#[arg(
@@ -19,6 +23,24 @@ pub struct TBACommand {
 		help = "Sets the custom config file to use."
 	)]
 	pub config: Option<PathBuf>,
+
+	#[arg(
+		short,
+		long,
+		global = true,
+		action = clap::ArgAction::Version,
+		help = "Prints version information."
+	)]
+	version: Option<bool>,
+
+	#[arg(
+		short,
+		long,
+		global = true,
+		action = clap::ArgAction::Help,
+		help = "Prints help information."
+	)]
+	help: Option<bool>,
 
 	#[command(subcommand)]
 	pub command: Option<TBASubcommand>,
