3d0ce81388c567624dd29b4374c8be2bf1fd52c4
Trevor Sears <trevor@trevorsears.com>
2026-08-14 16:07:51 -0400

    Alphabetized `TBASubcommand` variants.

--- src/app/scaffolding/tba_subcommand.rs
+++ src/app/scaffolding/tba_subcommand.rs
@@ -3,20 +3,20 @@ use crate::app::handlers::CLIGetRequest;
 #[derive(clap::Subcommand, Debug)]
 pub enum TBASubcommand {
 	#[command(
-		about = "Fetches information from the specified TBA API endpoint."
-	)]
-	Get {
-		#[clap(flatten)]
-		request: CLIGetRequest,
-	},
-
-	#[command(
 		about = "Generates an autocompletion script for a specified shell."
 	)]
 	Completions {
 		/// The shell for which to generate the autocompletion script.
 		#[arg(value_enum)]
 		shell: clap_complete::Shell,
+	},
+
+	#[command(
+		about = "Fetches information from the specified TBA API endpoint."
+	)]
+	Get {
+		#[clap(flatten)]
+		request: CLIGetRequest,
 	},
 
 	#[command(about = "Attempts to install autocompletion scripts for the \
