253241a8f4a62cf80d5ee2a6853f9614d544f4d9
Trevor Sears <trevor@trevorsears.com>
2026-08-14 20:58:16 -0400

    `tba completions` now optionally accepts a shell.

--- src/cli/handlers/print_completions.rs
+++ src/cli/handlers/print_completions.rs
@@ -2,16 +2,17 @@
 pub struct CLIPrintCompletionsCommand {
 	/// The shell for which to generate the autocompletion script.
 	#[arg(value_enum)]
-	pub shell: clap_complete::Shell,
+	pub shell: Option<clap_complete::Shell>,
 }
 
 pub fn print_completions(
 	command: CLIPrintCompletionsCommand,
 ) -> Result<(), String> {
 	use std::io::Write;
-	let completions = crate::cli::generate_completions(command.shell);
-	if let Err(error) = std::io::stdout().write_all(&completions) {
-		eprintln!("Error: {}", error);
+	let shell = crate::cli::determine_shell(command.shell)?;
+	let completions = crate::cli::generate_completions(shell);
+	match std::io::stdout().write_all(&completions) {
+		Ok(_) => Ok(()),
+		Err(e) => Err(format!("Failed to print completions to stdout: {e}")),
 	}
-	Ok(())
 }
