7f8150554d8bae45c1e9f728cf927f9c172faea0
Trevor Sears <trevor@trevorsears.com>
2026-08-15 00:20:47 -0400

    Added `include_defaults` flag to `tba config show`.

--- src/cli/handlers/config_show.rs
+++ src/cli/handlers/config_show.rs
@@ -2,7 +2,14 @@ use crate::cli::TBAConfig;
 
 #[derive(clap::Args, Debug, Clone)]
 pub struct CLIConfigShowCommandArgs {
-
+	#[arg(
+		long,
+		num_args = 0..=1,
+		require_equals = true,
+		default_missing_value = "true",
+		help = "Whether to include default values in the output."
+	)]
+	include_defaults: bool,
 }
 
 pub fn config_show(args: CLIConfigShowCommandArgs) -> Result<(), String> {
