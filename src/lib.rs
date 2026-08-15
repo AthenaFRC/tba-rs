a422ba095e18b1aac092064f8ce65fb71015fede
Trevor Sears <trevor@trevorsears.com>
2026-08-14 20:25:25 -0400

    Reordered and updated `lib` exports.

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
-pub mod models;
-
 mod api_client;
 pub use api_client::*;
 
 mod api_result;
 pub use api_result::*;
 
-#[macro_use]
-mod endpoints_macro;
-
-pub mod endpoints;
-
 #[cfg(feature = "cli")]
 pub mod cli;
+
+#[macro_use]
+mod endpoints_macro;
+pub(crate) use endpoints_macro::*;
+
+pub mod endpoints;
+pub mod models;
