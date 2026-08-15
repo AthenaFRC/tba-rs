b2e2c88cbae00adb13c895e73387b2f13f7ac3ac
Trevor Sears <trevor@trevorsears.com>
2026-08-15 00:24:29 -0400

    Re-fixed `OutputFormat` serialization and deserialization.

--- src/cli/output_format.rs
+++ src/cli/output_format.rs
@@ -1,14 +1,16 @@
 #[derive(
 	clap::ValueEnum, serde::Serialize, serde::Deserialize, Debug, Clone,
 )]
-#[clap(rename_all = "kebab-case")]
-#[serde(rename_all = "kebab-case")]
 pub enum OutputFormat {
 	/// Standard JSON format, without line breaks or indentation.
+	#[value(name = "json")]
+	#[serde(rename = "json")]
 	JSON,
 
 	/// Pretty-printed JSON format, including line breaks and indentation via
 	/// tabs.
+	#[value(name = "json-pretty-tabs")]
+	#[serde(rename = "json-pretty-tabs")]
 	JSONPrettyTabs,
 
 	/// Pretty-printed JSON format, including line breaks and indentation via 2
@@ -25,12 +27,18 @@ pub enum OutputFormat {
 
 	/// JSON Lines format. Each line of the resulting output will be a valid
 	/// JSON object.
+	#[value(name = "jsonl")]
+	#[serde(rename = "jsonl")]
 	JSONL,
 
 	/// Comma separative value format.
+	#[value(name = "csv")]
+	#[serde(rename = "csv")]
 	CSV,
 
 	/// Tab separated value format.
+	#[value(name = "tsv")]
+	#[serde(rename = "tsv")]
 	TSV,
 }
 
