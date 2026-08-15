01610da1712aa360754f47ad7a3cbd4044c4b0b1
Trevor Sears <trevor@trevorsears.com>
2026-08-14 22:55:53 -0400

    Fixed serialization and deserialization of `OutputFormat`.

--- src/cli/output_format.rs
+++ src/cli/output_format.rs
@@ -1,34 +1,35 @@
-#[derive(clap::ValueEnum, serde::Serialize, serde::Deserialize, Debug, Clone)]
+#[derive(
+	clap::ValueEnum, serde::Serialize, serde::Deserialize, Debug, Clone,
+)]
+#[clap(rename_all = "kebab-case")]
+#[serde(rename_all = "kebab-case")]
 pub enum OutputFormat {
 	/// Standard JSON format, without line breaks or indentation.
-	#[value(name = "json")]
 	JSON,
 
 	/// Pretty-printed JSON format, including line breaks and indentation via
 	/// tabs.
-	#[value(name = "json-pretty-tabs")]
 	JSONPrettyTabs,
 
 	/// Pretty-printed JSON format, including line breaks and indentation via 2
 	/// spaces.
 	#[value(name = "json-pretty-2spaces")]
+	#[serde(rename = "json-pretty-2spaces")]
 	JSONPretty2Spaces,
 
 	/// Pretty-printed JSON format, including line breaks and indentation via 4
 	/// spaces.
 	#[value(name = "json-pretty-4spaces")]
+	#[serde(rename = "json-pretty-4spaces")]
 	JSONPretty4Spaces,
 
 	/// JSON Lines format. Each line of the resulting output will be a valid
 	/// JSON object.
-	#[value(name = "jsonl")]
 	JSONL,
 
 	/// Comma separative value format.
-	#[value(name = "csv")]
 	CSV,
 
 	/// Tab separated value format.
-	#[value(name = "tsv")]
 	TSV,
 }
